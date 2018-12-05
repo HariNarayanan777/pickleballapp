a2cd497b3d9d92428c25c797729fac8e89c2d82b
Osmany Jirón <osmany@senorcoders.com>
2018-12-05 15:22:16 -0600

    packega.json

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { UpdateAccountComponent } from './update-account/update-account';
+import { IonicModule } from 'ionic-angular';
+import { CommonModule } from '@angular/common';
 @NgModule({
 	declarations: [UpdateAccountComponent],
-	imports: [],
+	imports: [IonicModule, CommonModule],
 	exports: [UpdateAccountComponent]
 })
 export class ComponentsModule {}
