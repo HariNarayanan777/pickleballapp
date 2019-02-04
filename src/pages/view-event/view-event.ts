544b2482f41a74bebcaf1d334503929047de6187
Osmany Jirón <osmany@senorcoders.com>
2019-02-04 17:34:09 -0600

    agregue find players  a home

--- src/pages/view-event/view-event.ts
+++ src/pages/view-event/view-event.ts
@@ -1,5 +1,5 @@
 import { Component, NgZone } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, ToastController, ActionSheetController } from 'ionic-angular';
 import { FormControl } from '@angular/forms';
 import { LiveComunicationProvider } from '../../providers/live-comunication/live-comunication';
 import { HelpersProvider } from '../../providers/helpers/helpers';
@@ -7,6 +7,8 @@ import { HttpClient } from '@angular/common/http';
 import { ViewCourtPage } from '../view-court/view-court';
 import { AuthProvider } from '../../providers/auth/auth';
 import { ViewTournamentPage } from '../view-tournament/view-tournament';
+import { RestProvider } from '../../providers/rest/rest';
+import { PublicProfilePage } from '../public-profile/public-profile';
 
 declare var google: any;
 
@@ -53,12 +55,14 @@ export class ViewEventPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public viewCtrl: ViewController, public http: HttpClient,
-    private zone: NgZone
+    private zone: NgZone, private rest: RestProvider, public toastCtrl: ToastController,
+    private actionSheetCtrl: ActionSheetController
   ) {
     this.type = this.navParams.get("type");
   }
 
   async ionViewWillEnter() {
+    this.userID = await AuthProvider.me.getIdUser();
     if (this.type === "courts")
       await this.getCourtsSaved();
     if (this.type === "tournaments")
@@ -70,6 +74,10 @@ export class ViewEventPage {
       return `url(./assets/imgs/pickleball-doubles.jpg)`;
     else if (this.type === "tournaments")
       return `url(assets/imgs/men-women-1.jpg)`;
+    else if(this.type === 'players')
+      return `url(assets/imgs/find-players.jpg)`;
+    else if(this.type === 'clinics')
+      return `url(assets/imgs/racket-balls.png)`;
   }
 
   public async changeDateStart() {
@@ -584,5 +592,139 @@ export class ViewEventPage {
   public toTournament(tournament) {
     this.navCtrl.push(ViewTournamentPage, { tournament, save: true });
   }
+  //#endregion
+
+  //#region para busquedad de players
+  onSearchInput() {
+    if (this.searchTerm === "") {
+      this.searching = false;
+      this.shouldShowCancel = false;
+    } else {
+      this.searching = true;
+      this.shouldShowCancel = true;
+      this.getdata();
+    }
+
+  }
+
+  getdata() {
+    this.rest.getData(`/users/finds/${this.userID}?name=${this.searchTerm}`).subscribe(
+      result => {
+        this.players = result;
+      },
+      err => {
+        console.error("Error : " + err);
+      });
+  }
+
+  public errorImage(e) {
+    e.target.src = "./assets/imgs/default-user.png";
+  }
+
+  public getUrlImage(player) {
+    if (player.loginFacebook) {
+      return player.loginFacebook.image;
+    }
+    return "";
+  }
+
+  cancel() {
+    this.searching = false;
+    this.players = [];
+  }
+
+
+  checkFriendRequest(player, $event) {
+    if (this.sendRequestAllow(player) === true) {
+      this.addFriend(player, $event);
+    }
+    let index = player.requests.findIndex(it => {
+      if (it.response === null) return true;
+      return false;
+    });
+    if (index !== -1)
+      this.cancelRequestActionSheet($event, player.requests[index]);
+  }
+
+  public addFriend(player, $event) {
+    let payload = {
+      from: this.userID,
+      to: player['_id']
+    }
+    this.rest.postData('/requestfriend', payload).subscribe(res => {
+      console.log("Request", res);
+      $event.srcElement.innerText = 'Cancel Request';
+      this.presentToast("Friendship Request sent!");
+      this.onSearchInput();
+    })
+  }
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  public sendRequestAllow(user) {
+    if (user.requests.length === 0) return true;
+    let indexResponseTrue = user.requests.findIndex(it => {
+      if (it.response === true || it.response === null) return true;
+      return false;
+    });
+    console.log("all", indexResponseTrue);
+    return indexResponseTrue === -1;
+  }
+
+  public isFriend(user) {
+    let isFriend = false;
+    for (let request of user.requests) {
+      if (request.response === true) {
+        isFriend = true;
+        break;
+      }
+
+    }
+
+    return !isFriend;
+  }
+
+  cancelRequestActionSheet($event, request) {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Request',
+      buttons: [
+        {
+          text: 'Cancel Request',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+            this.cancelRequest($event, request);
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  cancelRequest($event, request) {
+    this.rest.removeData('/requestfriend/' + request['id']).subscribe(res => {
+      this.onSearchInput();
+      $event.srcElement.innerText = 'Add Friend';
+      this.presentToast("Friendship Request cancelled!");
+      this.onSearchInput();
+    })
+  }
+
+  goToFriendProfile(userID){
+    this.navCtrl.push(PublicProfilePage, {'userID': userID});
+  }
+  //#endregion
 
 }
